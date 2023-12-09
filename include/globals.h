8c5ad86eac29a4b4c3790a9ce1c4054310ef373d
Marshall G. Gates <mggates@gmail.com>
2023-12-08 19:11:12 -0500

    Restore M5StickCPlus LED PIN Assignment

--- include/globals.h
+++ include/globals.h
@@ -279,7 +279,7 @@ extern RemoteDebug Debug;           // Let everyone in the project know about it
     #define ENABLE_OTA              0   // Accept over the air flash updates
 
     #if M5STICKC || M5STICKCPLUS || M5STACKCORE2
-        #define LED_PIN0 26
+        #define LED_PIN0 32
     #elif LILYGOTDISPLAYS3
         #define LED_PIN0 21
     #else
